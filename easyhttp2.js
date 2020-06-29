b66ad6e5a7c5fefc4214500c3e7941f9d59b847a
Julio Guimaraes <juliocefg@gmail.com>
2020-06-29 15:33:53 -0300

    first commit

---
+++ easyhttp2.js
@@ -0,0 +1,61 @@
+class EasyHTTP {
+    //  Make GET Request
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+
+    }
+
+    // Make POST Request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+
+    }
+    // Make PUT Request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+
+    }
+
+    // Make DELETE Request
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+                .then(res => res.json())
+                .then(() => resolve('User Deleted'))
+                .catch(err => reject(err))
+        })
+
+    }
+}
